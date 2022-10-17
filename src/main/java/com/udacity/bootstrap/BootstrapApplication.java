c6ddb8f5910fafab293c4a73165778aad4db93a8
Alex Brackenridge <a.brack@live.co.uk>
2022-10-17 15:12:22 +0100

    Modified application.properties. Added try catch to BootstrapApplication.java.

--- src/main/java/com/udacity/bootstrap/BootstrapApplication.java
+++ src/main/java/com/udacity/bootstrap/BootstrapApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootstrapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootstrapApplication.class, args);
-	}
+		try {
+			SpringApplication.run(BootstrapApplication.class, args);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		}
+
 
 }
